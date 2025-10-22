6b886fa32a48fbd5f22158d714ba35e35891c10a
gubFS <229577543+gubFS@users.noreply.github.com>
2025-10-22 22:34:45 +0200

    Add actions that show only relevant fields

--- pogo_object_panel.py
+++ pogo_object_panel.py
@@ -19,33 +19,53 @@ class PogoObjectPanel(bpy.types.Panel):
         obj = context.object
         if obj.type != 'MESH': return
 
+        entity = obj.pogo_entity
+
         layout.row().prop(obj, "name")
         layout.row().prop(obj, "location")
         layout.row().prop(obj, "rotation_euler", text="Rotation")
         layout.row().prop(obj, "scale")
-        layout.row().prop(obj.pogo_entity, "material")
+        layout.row().prop(entity, "material")
 
         row = layout.row()
-        row.prop(obj.pogo_entity, "ambient")
-        row.prop(obj.pogo_entity, "albedo")
+        row.prop(entity, "ambient")
+        row.prop(entity, "albedo")
 
         row = layout.row()
         col = row.column()
-        col.prop(obj.pogo_entity, "flag_invisible")
-        col.prop(obj.pogo_entity, "flag_unlit")
-        col.prop(obj.pogo_entity, "flag_transparent")
-        col.prop(obj.pogo_entity, "flag_polygon")
-        if obj.pogo_entity.flag_polygon == True:
-            col.prop(obj.pogo_entity, "flag_auto_collision")
+        col.prop(entity, "flag_invisible")
+        col.prop(entity, "flag_unlit")
+        col.prop(entity, "flag_transparent")
+        col.prop(entity, "flag_polygon")
+        if entity.flag_polygon == True:
+            col.prop(entity, "flag_auto_collision")
 
         col = row.column()
-        col.prop(obj.pogo_entity, "flag_shadow")
-        col.prop(obj.pogo_entity, "flag_cast")
-        col.prop(obj.pogo_entity, "flag_metal")
-        col.prop(obj.pogo_entity, "flag_7")
+        col.prop(entity, "flag_shadow")
+        col.prop(entity, "flag_cast")
+        col.prop(entity, "flag_metal")
+        col.prop(entity, "flag_7")
 
-        layout.row().prop(obj.pogo_entity, "action")
+        layout.row().prop(entity, "action")
+        if entity.action in entity.action_config:
+            currentConfig = entity.action_config.get(entity.action)
 
-        # for property in [attr for attr in dir(obj.pogo_entity) if not attr.startswith("__") and not attr in ["bl_rna", "name", "rna_type"]]:
+            if "flags" in currentConfig:
+                row = layout.row()
+                colLeft = row.column()
+                colRight = row.column()
+                for i, flag in enumerate(currentConfig["flags"]):
+                    col = colLeft if i % 2 == 0 else colRight
+                    # entity[flag["identifier"]] = flag["default"] # TODO: change value to default when action is changed (not on every draw)
+                    col.prop(entity, flag["identifier"], text=flag["name"])
+
+            if "skills" in currentConfig:
+                for i, skill in enumerate(currentConfig["skills"]):
+                    layout.row().prop(entity, skill["identifier"], text=skill["name"])
+
+            if "path" in currentConfig and currentConfig["path"] == True:
+                layout.row().prop(entity, "path")
+
+        # for property in [attr for attr in dir(entity) if not attr.startswith("__") and not attr in ["bl_rna", "name", "rna_type"]]:
         #     row = layout.row()
-        #     row.prop(obj.pogo_entity, property)
+        #     row.prop(entity, property)
--- wmb_classes.py
+++ wmb_classes.py
@@ -59,7 +59,459 @@ class PogoEntity(bpy.types.PropertyGroup):
     flag_polygon: bpy.props.BoolProperty(name="Collision") # = 26, # collision. if polygon isn't set then its passable
     ambient: bpy.props.FloatProperty(name="Ambient")
     albedo: bpy.props.FloatProperty(name="Albedo", default=50.0)
-    action: bpy.props.StringProperty(name="Action")
+
+    action_config = {
+        "mushroom_act": {
+            "flags": [
+                {
+                    "identifier": "flag_5",
+                    "name": "bounce_light",
+                    "default": False,
+                },
+            ],
+            "skills": [ ],
+        },
+        "mushroomFullObj_act": {
+            "flags": [ ],
+            "skills": [ ]
+        },
+        "execString12acts": {
+            "disable": True,
+            "flags": [ ],
+            "skills": [ ]
+        },
+        "skillset_act": {
+            "flags": [ ],
+            "skills": [
+                {
+                    "identifier": "skill_1",
+                    "name": "vecsk41",
+                    "default": 0.0,
+                },
+                {
+                    "identifier": "skill_2",
+                    "name": "vecsk42",
+                    "default": 0.0,
+                },
+                {
+                    "identifier": "skill_3",
+                    "name": "vecsk43",
+                    "default": 0.0,
+                },
+                {
+                    "identifier": "skill_4",
+                    "name": "vecsk44",
+                    "default": 0.0,
+                },
+                {
+                    "identifier": "skill_5",
+                    "name": "greyScale",
+                    "default": 0.0,
+                },
+                {
+                    "identifier": "skill_6",
+                    "name": "vecsk46",
+                    "default": 0.0,
+                },
+                {
+                    "identifier": "skill_7",
+                    "name": "vecsk47",
+                    "default": 0.0,
+                },
+                {
+                    "identifier": "skill_8",
+                    "name": "vecsk48",
+                    "default": 0.0,
+                },
+            ]
+        },
+        "slime_act": {
+            "flags": [ ],
+            "skills": [ ]
+        },
+        "POI_act": {
+            "flags": [
+                {
+                    "identifier": "flag_6",
+                    "name": "scary",
+                    "default": False,
+                },
+                {
+                    "identifier": "flag_7",
+                    "name": "mega",
+                    "default": False,
+                },
+            ],
+            "skills": [
+                {
+                    "identifier": "skill_1",
+                    "name": "vecsk41",
+                    "default": 0.0,
+                },
+                {
+                    "identifier": "skill_2",
+                    "name": "vecsk42",
+                    "default": 0.0,
+                },
+                {
+                    "identifier": "skill_3",
+                    "name": "vecsk43",
+                    "default": 0.0,
+                },
+                {
+                    "identifier": "skill_4",
+                    "name": "vecsk44",
+                    "default": 0.0,
+                },
+            ]
+        },
+        "monolithThorn_act": {
+            "flags": [ ],
+            "skills": [ ],
+            "path": True
+        },
+        "coconutSlippery": {
+            "flags": [ ],
+            "skills": [ ]
+        },
+        "pinkSap_act": {
+            "flags": [ ],
+            "skills": [ ]
+        },
+        "boostjuice_act": {
+            "flags": [ ],
+            "skills": [ ]
+        },
+        "map3Wheel_act": {
+            "flags": [
+                {
+                    "identifier": "flag_3",
+                    "name": "continuousRotation",
+                    "default": False,
+                },
+                {
+                    "identifier": "flag_4",
+                    "name": "sine_Rotation",
+                    "default": False,
+                },
+            ],
+            "skills": [
+                {
+                    "identifier": "skill_1",
+                    "name": "rotateSpeed",
+                    "default": 1.5,
+                },
+                {
+                    "identifier": "skill_2",
+                    "name": "sine_base",
+                    "default": 0.0,
+                },
+                {
+                    "identifier": "skill_3",
+                    "name": "sine_amount",
+                    "default": 45.0,
+                },
+            ]
+        },
+        "moveSine_act": {
+            "flags": [
+                {
+                    "identifier": "flag_3",
+                    "name": "y_changesAmbient",
+                    "default": False,
+                },
+            ],
+            "skills": [
+                {
+                    "identifier": "skill_1",
+                    "name": "x_dist",
+                    "default": 0.0,
+                },
+                {
+                    "identifier": "skill_2",
+                    "name": "z_dist",
+                    "default": 0.0,
+                },
+                {
+                    "identifier": "skill_3",
+                    "name": "x_speedfac",
+                    "default": 5.0,
+                },
+                {
+                    "identifier": "skill_4",
+                    "name": "z_speedfac",
+                    "default": 5.0,
+                },
+                {
+                    "identifier": "skill_5",
+                    "name": "x_AngOffset",
+                    "default": 0.0,
+                },
+                {
+                    "identifier": "skill_6",
+                    "name": "z_AngOffset",
+                    "default": 0.0,
+                },
+                {
+                    "identifier": "skill_7",
+                    "name": "y_dist",
+                    "default": 0.0,
+                },
+                {
+                    "identifier": "skill_8",
+                    "name": "y_speedfac",
+                    "default": 5.0,
+                },
+                {
+                    "identifier": "skill_9",
+                    "name": "y_AngOffset",
+                    "default": 0.0,
+                },
+                {
+                    "identifier": "skill_11",
+                    "name": "y_ambientEffect",
+                    "default": 0.25,
+                },
+            ]
+        },
+        "moveLoop_act": {
+            "flags": [ ],
+            "skills": [
+                {
+                    "identifier": "skill_1",
+                    "name": "x_dist",
+                    "default": 0.0,
+                },
+                {
+                    "identifier": "skill_2",
+                    "name": "z_dist",
+                    "default": 0.0,
+                },
+                {
+                    "identifier": "skill_3",
+                    "name": "speedfac",
+                    "default": 5.0,
+                },
+                {
+                    "identifier": "skill_5",
+                    "name": "angOffset",
+                    "default": 0.0,
+                },
+            ]
+        },
+        "toggleBlock_act": {
+            "flags": [
+                {
+                    "identifier": "flag_3",
+                    "name": "boost_toggle",
+                    "default": False,
+                },
+                {
+                    "identifier": "flag_4",
+                    "name": "group_2",
+                    "default": False,
+                },
+                {
+                    "identifier": "flag_5",
+                    "name": "turn_invisible",
+                    "default": False,
+                },
+            ],
+            "skills": [
+                {
+                    "identifier": "skill_1",
+                    "name": "redBlueTintFac",
+                    "default": 1.0,
+                },
+                {
+                    "identifier": "skill_2",
+                    "name": "dither_scale",
+                    "default": 1.0,
+                },
+                {
+                    "identifier": "skill_3",
+                    "name": "dither_fac",
+                    "default": 0.65,
+                },
+            ]
+        },
+        "customMapSetup_act": {
+            "disable": True,
+            "flags": [
+                {
+                    "identifier": "flag_3",
+                    "name": "double_jump",
+                    "default": False,
+                },
+                {
+                    "identifier": "flag_4",
+                    "name": "puzzle",
+                    "default": False,
+                },
+                {
+                    "identifier": "flag_5",
+                    "name": "no_boost",
+                    "default": False,
+                },
+                {
+                    "identifier": "flag_6",
+                    "name": "bonk_explode",
+                    "default": False,
+                },
+                {
+                    "identifier": "flag_7",
+                    "name": "mushroom_power",
+                    "default": False,
+                },
+                {
+                    "identifier": "flag_8",
+                    "name": "iceMode",
+                    "default": False,
+                },
+            ],
+            "skills": [ ]
+        },
+        "toggleSine_act": {
+            "flags": [
+                {
+                    "identifier": "flag_3",
+                    "name": "turn_background",
+                    "default": False,
+                },
+                {
+                    "identifier": "flag_5",
+                    "name": "turn_invisible",
+                    "default": False,
+                },
+            ],
+            "skills": [
+                {
+                    "identifier": "skill_1",
+                    "name": "speedFac",
+                    "default": 5.0,
+                },
+                {
+                    "identifier": "skill_2",
+                    "name": "offset",
+                    "default": 0.0,
+                },
+                {
+                    "identifier": "skill_3",
+                    "name": "ambient_shift",
+                    "default": -35.0,
+                },
+            ]
+        },
+        "bgObject_act": {
+            "flags": [
+                {
+                    "identifier": "flag_6",
+                    "name": "bottom_z",
+                    "default": False,
+                },
+                {
+                    "identifier": "flag_7",
+                    "name": "fixed_x",
+                    "default": False,
+                },
+                {
+                    "identifier": "flag_8",
+                    "name": "fixed_z",
+                    "default": False,
+                },
+            ],
+            "skills": [
+                {
+                    "identifier": "skill_1",
+                    "name": "fac_x",
+                    "default": 1.0,
+                },
+                {
+                    "identifier": "skill_2",
+                    "name": "fac_z",
+                    "default": 1.0,
+                },
+                {
+                    "identifier": "skill_3",
+                    "name": "greyScale",
+                    "default": 0.0,
+                },
+                {
+                    "identifier": "skill_5",
+                    "name": "skill43",
+                    "default": 0.0,
+                },
+            ]
+        },
+        "runStateToggle": {
+            "flags": [
+                {
+                    "identifier": "flag_3",
+                    "name": "show_start",
+                    "default": False,
+                },
+                {
+                    "identifier": "flag_4",
+                    "name": "show_sun",
+                    "default": False,
+                },
+                {
+                    "identifier": "flag_5",
+                    "name": "show_end",
+                    "default": False,
+                },
+                {
+                    "identifier": "flag_6",
+                    "name": "turn_passable",
+                    "default": False,
+                },
+                {
+                    "identifier": "flag_7",
+                    "name": "fade_scale",
+                    "default": False,
+                },
+            ],
+            "skills": [
+                {
+                    "identifier": "skill_1",
+                    "name": "fade_speed",
+                    "default": 10.0,
+                },
+                {
+                    "identifier": "skill_2",
+                    "name": "dofade_sk41",
+                    "default": 0.0,
+                },
+                {
+                    "identifier": "skill_3",
+                    "name": "dofade_alpha",
+                    "default": 0.0,
+                },
+                {
+                    "identifier": "skill_4",
+                    "name": "dosmoothstep",
+                    "default": 0.0,
+                },
+                {
+                    "identifier": "skill_5",
+                    "name": "doovershoot",
+                    "default": 0.0,
+                },
+                {
+                    "identifier": "skill_6",
+                    "name": "overshoot_fac",
+                    "default": 1.35,
+                },
+            ]
+        }
+    }
+
+    actions = [("ndef", "", "")]
+    for action, config in action_config.items():
+        if "disable" in config and config["disable"] == True: continue
+        actions.append((action, config["name"] if "name" in config else action, config["description"] if "description" in config else ""))
+    action: bpy.props.EnumProperty(items=actions,name="Action", default="ndef")
+
     flag_1: bpy.props.BoolProperty(name="flag_1") # = 0,
     flag_2: bpy.props.BoolProperty(name="flag_2") # = 1,
     flag_3: bpy.props.BoolProperty(name="flag_3") # = 2,
