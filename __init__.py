23a0c29d8b2d7cfbe7e7730399aba5bd2c11d791
gubFS <229577543+gubFS@users.noreply.github.com>
2025-10-24 19:05:51 +0200

    Add auto reloading of all python files
    Blender is dumb and doesn't reload everything so this does the job

--- __init__.py
+++ __init__.py
@@ -1,7 +1,22 @@
+import os
+import importlib
+
 if "bpy" in locals():
-    import importlib
-    if "pogo_blend" in locals():
-        importlib.reload(pogo_blend)
+    for root, dirs, files in os.walk(os.path.dirname(__file__)):
+        if "__pycache__" in root or ".git" in root: continue
+        for file in files:
+            filename, file_extension = os.path.splitext(file)
+            if file_extension != ".py": continue
+            if file == "__init__.py": continue
+            if filename in locals():
+                importlib.reload(locals()[filename])
+                print(f"reloaded: {filename}")
+            else:
+                rel = root.split("/pogo_blend")[1].replace("/", ".")
+                rel = f"{rel}.{filename}"
+                locals()[filename] = importlib.import_module(rel, package=__name__)
+                print(f"loaded: {filename}")
+
 import bpy
 from . import pogo_blend
 
--- pogo_blend.py
+++ pogo_blend.py
@@ -1,23 +1,15 @@
-if "bpy" in locals():
-    import importlib
-    if "wmb_classes" in locals():
-        importlib.reload(wmb_classes)
-    if "pogo_object_panel" in locals():
-        importlib.reload(pogo_object_panel)
-    if "pogo_collection_panel" in locals():
-        importlib.reload(pogo_collection_panel)
 import bpy
 
-from . import wmb_classes
+from . import pogo_classes
 from . import pogo_object_panel
 from . import pogo_collection_panel
 
 def register():
-    wmb_classes.register()
+    pogo_classes.register()
     pogo_object_panel.register()
     pogo_collection_panel.register()
 
 def unregister():
-    wmb_classes.unregister()
+    pogo_classes.unregister()
     pogo_object_panel.unregister()
     pogo_collection_panel.unregister()
