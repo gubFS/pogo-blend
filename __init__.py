782a58f0d3dffdb0c4f12f1f96943fb68b5775e6
gubFS <229577543+gubFS@users.noreply.github.com>
2025-11-07 08:47:18 +0100

    Add automatic registering
    Any module with a register and unregister function will be called

--- __init__.py
+++ __init__.py
@@ -1,28 +1,35 @@
 import os
 import importlib
 
-if "bpy" in locals():
-    for root, dirs, files in os.walk(os.path.dirname(__file__)):
-        if "__pycache__" in root or ".git" in root: continue
-        for file in files:
-            filename, file_extension = os.path.splitext(file)
-            if file_extension != ".py": continue
-            if file == "__init__.py": continue
-            if filename in locals():
-                importlib.reload(locals()[filename])
-                print(f"reloaded: {filename}")
-            else:
-                rel = root.split("/pogo_blend")[1].replace("/", ".")
-                rel = f"{rel}.{filename}"
-                locals()[filename] = importlib.import_module(rel, package=__name__)
-                print(f"loaded: {filename}")
+modules = []
+for root, dirs, files in os.walk(os.path.dirname(__file__)):
+    if "__pycache__" in root or ".git" in root: continue
+    for file in files:
+        filename, file_extension = os.path.splitext(file)
+        if file_extension != ".py": continue
+        if file == "__init__.py": continue
+        if filename in locals():
+            importlib.reload(locals()[filename])
+            modules.append(locals()[filename])
+            # print(f"reloaded: {filename}")
+        else:
+            rel = root.split("/pogo_blend")[1].replace("/", ".")
+            rel = f"{rel}.{filename}"
+            locals()[filename] = importlib.import_module(rel, package=__name__)
+            modules.append(locals()[filename])
+            # print(f"loaded: {filename}")
 
 import bpy
-from . import pogo_blend
 
 def register():
-    pogo_blend.register()
+    for module in modules:
+        register = getattr(module, "register", None)
+        if callable(register):
+            module.register()
 
 def unregister():
-    pogo_blend.unregister()
+    for module in modules:
+        unregister = getattr(module, "unregister", None)
+        if callable(unregister):
+            module.unregister()
 
