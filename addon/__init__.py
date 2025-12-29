0344e24bab320f8191a1a37ce1a4393b7c2f4d95
gubFS <229577543+gubFS@users.noreply.github.com>
2025-12-29 12:24:15 +0100

    Update init file

--- addon/__init__.py
+++ addon/__init__.py
@@ -1,37 +1,59 @@
 import importlib
 import os
 
-modules = []
-for root, dirs, files in os.walk(os.path.dirname(__file__)):
-    if "__pycache__" in root or ".git" in root:
-        continue
-    for file in files:
-        filename, file_extension = os.path.splitext(file)
-        if file_extension != ".py":
-            continue
-        if file == "__init__.py":
-            continue
-        if filename in locals():
-            importlib.reload(locals()[filename])
-            modules.append(locals()[filename])
-            # print(f"reloaded: {filename}")
-        else:
-            rel = root.split("/pogo_blend")[1].replace("/", ".")
-            rel = f"{rel}.{filename}"
-            locals()[filename] = importlib.import_module(rel, package=__name__)
-            modules.append(locals()[filename])
-            # print(f"loaded: {filename}")
+from . import pogo_blend_preferences as pbu
+from .export_import import custom_map_builder_operator
+from .export_import.mdl_exporter import mdl_exporter_operator
+from .export_import.mdl_importer import mdl_importer_operator
+from .pogo_classes import pogo_custom_map, pogo_entity, pogo_path, pogo_reigon
+from .ui import add_pogo_menu, pogo_collection_panel, pogo_object_panel
+from .ui.gizmos import gravity_gizmo
+from .ui.operators import (add_block, add_pogo_path, add_pogo_reigon,
+                           add_sprite, create_collider)
 
+# Uncomment the following for quick reloading of addon. Only for use in development.
+# for root, dirs, files in os.walk(os.path.dirname(__file__)):
+#     root.replace("\\", "/") # Windows >:(
+#     if "__pycache__" in root or ".git" in root:
+#         continue
+#     for file in files:
+#         filename, file_extension = os.path.splitext(file)
+#         if file_extension != ".py":
+#             continue
+#         if file == "__init__.py":
+#             continue
+#         if filename in locals():
+#             importlib.reload(locals()[filename])
+#         else:
+#             rel = root.split("/pogo_blend")[1].replace("/", ".")
+#             rel = f"{rel}.{filename}"
+#             locals()[filename] = importlib.import_module(rel, package=__name__)
+
+to_register = [
+    pbu, 
+    custom_map_builder_operator, 
+    mdl_exporter_operator, 
+    mdl_importer_operator, 
+    pogo_custom_map, 
+    pogo_entity, 
+    pogo_path, 
+    pogo_reigon, 
+    add_pogo_menu, 
+    pogo_collection_panel, 
+    pogo_object_panel, 
+    gravity_gizmo, 
+    add_block, 
+    add_pogo_path, 
+    add_pogo_reigon, 
+    add_sprite, 
+    create_collider,
+]
 
 def register():
-    for module in modules:
-        register = getattr(module, "register", None)
-        if callable(register):
-            module.register()
+    for module in to_register:
+        module.register()
 
 
 def unregister():
-    for module in modules:
-        unregister = getattr(module, "unregister", None)
-        if callable(unregister):
-            module.unregister()
+    for module in to_register:
+        module.unregister()
