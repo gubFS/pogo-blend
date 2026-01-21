a077c368d774e96aa67b588a2813b5c7bab0e07a
gubFS <229577543+gubFS@users.noreply.github.com>
2026-01-21 16:01:08 +0100

    Don't change sprite names from their file name

--- addon/ui/operators/add_sprite.py
+++ addon/ui/operators/add_sprite.py
@@ -11,16 +11,13 @@ class AddSprite(bpy.types.IMAGE_OT_import_as_mesh_planes):
         ret_code = super().execute(context)
 
         if ret_code == {'FINISHED'}:
-            sprite = context.object
-
             bpy.ops.object.editmode_toggle()
             bpy.ops.mesh.select_all(action='SELECT')
             bpy.ops.transform.rotate(value=1.5708, orient_axis='X', orient_type='GLOBAL', orient_matrix=((1, 0, 0), (0, 1, 0), (0, 0, 1)), orient_matrix_type='GLOBAL', constraint_axis=(True, False, False), mirror=True)
             bpy.ops.object.editmode_toggle()
-
-            sprite.name = "PogoSprite"
-            entity = sprite.pogo_entity
-            entity.material = "cmUnlit"
+            for obj in context.selected_objects:
+                entity = obj.pogo_entity
+                entity.material = "cmUnlit"
 
         return ret_code
 
