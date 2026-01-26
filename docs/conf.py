4c5ed202b8a57a8e434af0276dba16dc2a9bac18
gubFS <229577543+gubFS@users.noreply.github.com>
2026-01-26 15:31:55 +0100

    Update documentation config

--- docs/conf.py
+++ docs/conf.py
@@ -16,6 +16,7 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 html_title = 'PogoBlend'
 html_theme = 'furo'
 html_static_path = ['_static']
+html_copy_source = False
 
 html_theme_options: dict[str, Any] = {
     "footer_icons": [
@@ -30,7 +31,4 @@ html_theme_options: dict[str, Any] = {
             "class": "",
         },
     ],
-    "source_repository": "https://github.com/gubFS/pogo-blend",
-    "source_branch": "main",
-    "source_directory": "docs/",
 }
